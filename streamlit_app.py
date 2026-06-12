1fc313be28f2db20853efe394df1b1d0afc97096
JayanthsaiAdari <62716857+JayanthsaiAdari@users.noreply.github.com>
2026-06-12 15:31:37 +0530

    Comment out st.stop() and st.write()
    Comment out stop and write statements in the Streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 #Convert  the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:',
@@ -46,7 +46,7 @@ if ingredients_list:
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
         
-    st.write(ingredients_string)
+    #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
